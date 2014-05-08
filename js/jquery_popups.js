8b56b1111c666d51ef58237ecc043adfc9a86d90
Beamanator <abeaman@mines.edu>
2014-05-07 22:13:32 -0600

    More Stuff

--- js/jquery_popups.js
+++ js/jquery_popups.js
@@ -1,8 +1,27 @@
 // Waits for all HTML to be written & document to be ready.
 $(document).ready(function() {
   // $('div#type1 > h4').click(function() {
-  $("h4").click(function() {
-    $("div").find("p").css("background-color","red");
+    var callable = true;
+  $("#jq").mouseenter(function() {
+    // $("div").find("p").css("background-color","red");
+    if (callable) {
+      $('body').fadeTo('slow',0.5);
+      callable = false;
+    }
+
     //$('> p').fadeOut(1000);
+  });
+  $('#jq').mouseout(function() {
+    $('body').fadeTo('slow',1, function() {
+      callable = true;
+    });
+  });
+  $('#try1').click(function() {
+    //location.reload();
+    //$('test').removeAttr('style');
+    $('#test').css({
+      "display":"block"
+    });
+    location.reload();
   });
 });
